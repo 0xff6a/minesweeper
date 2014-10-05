e2683046c11af87c4ae08bb137f1541683a2c05f
Jeremy Fox <foxjerem@gmail.com>
2014-10-05 01:03:19 +0100

    Refactor

--- src/HiddenContent.js
+++ src/HiddenContent.js
@@ -1,15 +1,14 @@
 function HiddenContent() {};
 
-function Blank() {};
-
-function Mine() {};
-
 HiddenContent.prototype.display = function() {
     return "hidden";
 };
 
+function Blank() {};
 Blank.prototype = new HiddenContent;
 Blank.prototype.constructor = Blank;
+
+function Mine() {};
 Mine.prototype = new HiddenContent;
 Mine.prototype.constructor = Mine;
 
